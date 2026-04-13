db157ef3bf633de38725fb0ec88177c2f2a5113e
Shubham Anand <shubhamanand799@gmail.com>
2026-04-13 16:32:27 +0530

    Again config

--- config/db_config.py
+++ config/db_config.py
@@ -4,21 +4,26 @@ import streamlit as st
 
 
 def get_connection():
-    host = st.secrets["SUPABASE_DB_HOST"]
+    try:
+        host = st.secrets["SUPABASE_DB_HOST"]
 
-    # Force IPv4 resolution
-    ipv4 = socket.getaddrinfo(host, 5432, socket.AF_INET)[0][4][0]
+        print("Using host:", host)
 
+        # Force IPv4
+        ipv4 = socket.getaddrinfo(host, 5432, socket.AF_INET)[0][4][0]
 
-
-    conn =  psycopg2.connect(
-        host=ipv4,
-        database="postgres",
-        user="postgres",
-        password="Shubham9746",
-        port="5432",
-        sslmode="require"
+        conn = psycopg2.connect(
+            host=ipv4,
+            database="postgres",
+            user="postgres",
+            password=st.secrets["SUPABASE_DB_PASSWORD"],  # 🔐 move to secrets
+            port=5432,
+            sslmode="require"
         )
-    return conn
 
-print("✅ Connected to CLOUD PostgreSQL")
+        print("✅ Connected")
+        return conn
+
+    except Exception as e:
+        print("❌ Error:", e)
+        return None
