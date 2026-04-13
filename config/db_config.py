80ac302dc17295f82fbfd3fefa11b4075f00d0ba
Shubham Anand <shubhamanand799@gmail.com>
2026-04-13 16:13:52 +0530

    update psotgres ipv4

--- config/db_config.py
+++ config/db_config.py
@@ -1,12 +1,23 @@
 import psycopg2
+import socket
 
 
 def get_connection():
-    return psycopg2.connect(
-        host="db.fsjhbqnzefptdzbkrjck.supabase.co",
+    host = "db.fsjhbqnzefptdzbkrjck.supabase.co"
+
+    # Force IPv4 resolution
+    ipv4 = socket.getaddrinfo(host, 5432, socket.AF_INET)[0][4][0]
+
+
+
+    conn =  psycopg2.connect(
+        host=ipv4,
         database="postgres",
         user="postgres",
         password="Shubham9746",
-        port="5432")
+        port="5432",
+        sslmode="require"
+        )
+    return conn
 
 print("✅ Connected to CLOUD PostgreSQL")
